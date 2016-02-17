6129aeb35f36805e73c16ec83d22736dd116aaee
Frederico <frederico_assis@hotmail.com>
2016-02-17 16:04:51 -0200

    criação da funcão somaDosGraus.

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -26,5 +26,72 @@
 	        
 	        return true;
 	    }
+
+	    public static function somaDosGraus(Graph $g){
+	        
+	        $allArestas = $g->getEdgeSet();
+
+			if(count($allArestas) == 1){
+	        	return 2;
+	        }
+	        else{
+	            $edge = self::anyEdge($g); 
+	            $h = self::graphMinusEdge($g, $edge);
+	            return ( 2 + self::somaDosGraus($h) );
+	        } 
+	       	                
+	    }
+
+	    public static function graphMinusEdge(Graph $g, Edge $edge){
+
+	    	$newName = $g->getName()." - e";
+	    	$h = new Graph($newName);
+
+	    	$allVertices = $g->getVertexSet();
+	    	$h->setVertexSet($allVertices);	
+	    	
+	    	$allEdges = $g->getEdgeSet();
+	    	foreach ($allEdges as $e) {
+				$endPoints = $e->getEndPoints();
+				$source = $endPoints[0];
+				$target = $endPoints[1];
+
+				if( !($edge->contains($souce) && $edge->contains($target)) )
+					$h->addEdge($source, $target);
+	    	}    	
+
+	    	return $h;
+	    }
+
+	    public static function anyEdge(Graph $g){
+
+	    	$edgeSet = $g->getEdgeSet();
+	    	$size = count($edgeSet);
+
+	    	if($size != 0){		    	
+		    	$position = rand(0, $size-1);
+		    	return $edgeSet[$position];
+		    }
+
+		    return null;
+	    }
+
+	    public static function ArestasArvore(Graph $g){
+	        
+	        $allVertices = $g->getVertexSet();
+
+	        
+	        
+	            if($allVertices == 1){
+	                
+	                    return 0;
+	            }
+	            else $degree = $g->degreeOf($vertex);
+	        
+	        
+	        return (1+ ArestasArvore());
+	    }
+
+
 	}
 ?>
